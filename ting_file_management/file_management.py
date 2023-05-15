3a6d0ee11ca74d4c2d35c48c00d2f3aa34c06326
Milena Suzumura <milena.suzumura11@gmail.com>
2023-05-15 19:39:21 -0300

    requisito 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if path_file.endswith(".txt"):
+        try:
+            with open(path_file, 'r') as f:
+                return f.read().split("\n")
+
+        except FileNotFoundError:
+            sys.stderr.write("Arquivo {path_file} não encontrado\n".format(
+                path_file=path_file))
+
+    else:
+        sys.stderr.write("Formato inválido")
